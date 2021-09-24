9d76758ed19a01b4b471cb96dea701571ae91039
rrwinterton <rrwinterton@gmail.com>
2021-09-24 10:21:43 -0600

    Initial commit

---
+++ maininit.js
@@ -0,0 +1,37 @@
+//initialize
+function Initialize() {
+    //check for browser supports service worker (sw) and register sw
+    if ('serviceWorker' in navigator) {
+        //is browser supports sw register sw
+        navigator.serviceWorker.register('/pwawasm/sw.js', {
+            scope: './'
+        }).then(function (registration) {
+            var serviceWorker;
+            if (registration.installing) {
+                serviceWorker = registration.installing;
+            } else if (registration.waiting) {
+                serviceWorker = registration.waiting;
+            } else if (registration.active) {
+                serviceWorker = registration.active;
+            }
+            //future update of push notifications
+/*            
+            if (serviceWorker) {
+                if ('PushManager' in window) {
+                    console.log('Push is supported');
+                } else {
+                    console.warn('Push messaging is not supported');
+                    pushButton.textContent = 'Push Not Supported';
+                }
+                serviceWorker.addEventListener('statechange', function (e) {
+                });
+            }
+*/            
+        }).catch(function (error) {
+        });       
+    }    
+}
+
+//initialization and install service worker (sw)
+Initialize();
+
---
+++ sw.js
@@ -0,0 +1,61 @@
+//simple service worker
+
+//precache version information
+const PRECACHE = 'precache-v1';
+//runtime version information
+const RUNTIME = 'runtime-v1';
+
+//resources
+const PRECACHE_URLS = [
+  "/pwawasm/maininit.js",
+  "/pwawasm/main.js",
+  "/pwawasm/index.html",
+  "/pwawasm/main.wasm",
+  "/pwawasm/manifest.json",
+  "/pwawasm/main.js.mem",
+  "/pwawasm/pwa-192.png",
+  "/pwawasm/pwa-512.png",
+];
+
+//add pwa install handler and precache urls
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+//runtime active handler
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+//fetch handler
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
